662e03e4326f5f8b65eda82d09fcbef3a62aefbd
igorpav <Igor.Pavlichenkov@tatar.ru>
2020-01-27 00:51:56 +0300

    wow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import {Redirect, Route, Switch} from "react-router";
+import PrivateRoute from "./Components/privateRoute";
+import LaginPage from "./Components/Pages/loginPage";
+import MainPage from "./Components/Pages/MainPage";
 
-function App() {
+const AUTH_TOKEN = 'auth-token';
+
+const App = () => {
+
+  let [token, updateToken] = useState(localStorage.getItem(AUTH_TOKEN));
+
+  const login = (newToken) => {
+    localStorage.setItem(AUTH_TOKEN, newToken);
+    updateToken(localStorage.getItem(AUTH_TOKEN));
+  };
+
+  const logout = () => {
+    localStorage.removeItem(AUTH_TOKEN);
+    updateToken(localStorage.getItem(AUTH_TOKEN));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Redirect from="/" to="" />
+      <Switch>
+        <Route path="/login">
+          <LaginPage login={login}/>
+        </Route>
+        <PrivateRoute path="" token={token}>
+          <Header token={token} logout={logout}/>
+          <MainPage/>
+        </PrivateRoute>
+      </Switch>
     </div>
-  );
-}
+  )};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {setContext} from "apollo-link-context";
+import { ApolloClient } from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import grey from "@material-ui/core/colors/grey";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: grey,
+    secondary: grey.A900,
+    },
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const AUTH_TOKEN = 'auth-token';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
+
 serviceWorker.unregister();
