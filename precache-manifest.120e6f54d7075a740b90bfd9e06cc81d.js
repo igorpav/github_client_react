9f540caf69c5325c49c58b444c4734654b24ce1a
igorpav <Igor.Pavlichenkov@tatar.ru>
2020-01-27 00:59:49 +0300

    Updates

---
+++ precache-manifest.120e6f54d7075a740b90bfd9e06cc81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f412df91983d0a35f4a65c25d8b6f3",
+    "url": "/github_client_react/index.html"
+  },
+  {
+    "revision": "6e689367a0176dcf241c",
+    "url": "/github_client_react/static/css/main.6668ef2d.chunk.css"
+  },
+  {
+    "revision": "59d4930c763709a20609",
+    "url": "/github_client_react/static/js/2.939735dc.chunk.js"
+  },
+  {
+    "revision": "bf9b132abb09dd0ca87ab270fe5de5f9",
+    "url": "/github_client_react/static/js/2.939735dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e689367a0176dcf241c",
+    "url": "/github_client_react/static/js/main.dabb1a79.chunk.js"
+  },
+  {
+    "revision": "8930c94eeb8cb19c20e8",
+    "url": "/github_client_react/static/js/runtime-main.2a202e94.js"
+  }
+]);
